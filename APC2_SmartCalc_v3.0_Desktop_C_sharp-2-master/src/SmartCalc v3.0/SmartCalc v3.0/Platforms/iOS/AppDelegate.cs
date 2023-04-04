00f9226f3cda52214d909af858b580aecc2b6475
Audria Jhin <ajhin@student.21-school.ru>
2023-04-04 16:50:08 +0700

    init

---
+++ APC2_SmartCalc_v3.0_Desktop_C_sharp-2-master/src/SmartCalc v3.0/SmartCalc v3.0/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SmartCalc_v3._0;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
