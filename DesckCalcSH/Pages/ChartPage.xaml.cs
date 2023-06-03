32f42d5ee7db02fff314c3998ceeef1ff39b7f08
Tvix57 <55078980+Tvix57@users.noreply.github.com>
2023-06-03 13:35:26 +0700

    fix graph

--- DesckCalcSH/Pages/ChartPage.xaml.cs
+++ DesckCalcSH/Pages/ChartPage.xaml.cs
@@ -12,7 +12,7 @@ public partial class ChartPage : ContentPage
 	{
         InitializeComponent();
     }
-    private void DrowGraph(object sender, EventArgs e)
+    async private void DrowGraph(object sender, EventArgs e)
     {
         if (XminField.Text != null &&
             Regex.IsMatch(XminField.Text, @"^[+-]?\d+(\.\d+)?$")) 
@@ -23,13 +23,13 @@ public partial class ChartPage : ContentPage
                 if (StepField.Text != null &&
                     Regex.IsMatch(StepField.Text, @"^\d+(\.\d+)?$")) 
                 {
-                    if (_model != null) 
+                    if (_model != null)
                     {
                         double xMin, xMax, step;
                         Double.TryParse(XminField.Text, out xMin);
                         Double.TryParse(XmaxField.Text, out xMax);
                         Double.TryParse(StepField.Text, out step);
-                        if (xMin > xMax) 
+                        if (xMin > xMax)
                         {
                             double tmp = xMax;
                             xMax = xMin;
@@ -37,28 +37,32 @@ public partial class ChartPage : ContentPage
                             XminField.Text = xMin.ToString();
                             XmaxField.Text = xMax.ToString();
                         }
-                        if (xMin < -1000000f) 
+                        if (xMin < -1000000d)
                         {
-                            xMin = -1000000f;
+                            xMin = -1000000d;
                             XminField.Text = xMin.ToString();
                         }
-                        if (xMax > 1000000f)
+                        if (xMax > 1000000d)
                         {
-                            xMax = 1000000f;
+                            xMax = 1000000d;
                             XmaxField.Text = xMax.ToString();
                         }
-                        if (step > xMin+xMax)/////
+                        if (step > Math.Abs(xMin) + Math.Abs(xMax))
                         {
-                           // step = xMin + xMax;
-                           // XminField.Text = step.ToString();
+                            step = Math.Abs(xMin) + Math.Abs(xMax);
+                            StepField.Text = step.ToString();
                         }
-
                         var CVM = ChartViewLink.BindingContext as ChartViewModel;
                         CVM.ClearAll();
                         for (; xMin < xMax; xMin += step)
                         {
                             CVM.AddValue(xMin, _model.Calculate(xMin));
                         }
+                        CVM.AddValue(xMax, _model.Calculate(xMax));
+                    }
+                    else 
+                    {
+                        await DisplayAlert("Alert", "Model dont load. Please input correct model on calculator page", "OK");
                     }
                 } 
                 else
