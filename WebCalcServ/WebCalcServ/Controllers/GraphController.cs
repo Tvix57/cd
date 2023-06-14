df6507cba63af6ab05bbbf9ced057fdb3e2dd4d0
Audria Jhin <ajhin@student.21-school.ru>
2023-06-14 18:43:05 +0700

    add js script and btns

---
+++ WebCalcServ/WebCalcServ/Controllers/GraphController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebCalcServ.Models;
+
+namespace WebCalcServ.Controllers;
+
+public class GraphController : Controller
+{
+    private readonly ILogger<GraphController> _logger;
+
+    public GraphController(ILogger<GraphController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Calculator()
+    {
+        return View();
+    }
+
+    public IActionResult Graph()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
--- WebCalcServ/WebCalcServ/wwwroot/js/site.js
+++ WebCalcServ/WebCalcServ/wwwroot/js/site.js
@@ -3,3 +3,139 @@
 
 // Write your JavaScript code.
 
+
+function AddText(inputText) {
+    var lab = document.getElementById("ResultLine");
+    lab.innerHTML += inputText;
+}
+
+function AddNum(buttonText) {
+    //if (_input.Length == 0 || Regex.IsMatch(_input, @"(\+\\\-|[\+\-\*/^\(\.]|mod|\d)$"))
+
+    if (true) {
+        AddText(buttonText);
+    }
+}
+
+function AddOperator(buttonText) {
+
+    //if (Regex.IsMatch(text, @"^([\*/^]|mod)$"))
+    //{
+    //    if (Regex.IsMatch(_input, @"([)x]|\d)$"))
+    //    {
+    //        Input += text;
+    //    }
+    //}
+    //        else
+    //{
+    //    if (!Regex.IsMatch(_input, @"(([\+\\\-\*\/\^\(\)]|mod)[\+\\\-])$") && _input.Length != 1)
+    //    {
+    //        Input += text;
+    //    }
+    //            else if (Regex.IsMatch(_input, @"([)xX]|\d)$"))
+    //    {
+    //        Input += text;
+    //    }
+    //}
+
+
+    if (true) {
+        AddText(buttonText);
+    }
+}
+
+function AddFunc(buttonText) {
+    //if (_input.Length == 0 || Regex.IsMatch(_input, @"([\+\-\*/^(]|mod)$"))
+    if (true) {
+        AddText(buttonText);
+        AddOBranch();
+    }
+}
+
+function ClearAll() {
+    var lab = document.getElementById("ResultLine");
+    //branches = 0;
+    lab.innerHTML = "";
+}
+
+function RemoveLast() {
+
+
+    //string text = _input;
+    //if (Regex.IsMatch(text, @"(\d|[\.\+\-\*\/\)xX])$"))
+    //{
+    //    if (text.Last() == ')') {
+    //        branches++;
+    //    }
+
+    //    text = text.Remove(text.Length - 1, 1);
+    //}
+    //        else if (Regex.IsMatch(text, @"[\(]$"))
+    //{
+    //    branches--;
+    //    text = text.Remove(text.Length - 1, 1);
+    //    if (Regex.IsMatch(text, @"(ln)$"))
+    //    {
+    //        text = text.Remove(text.Length - 2, 2);
+    //    }
+    //            else if (text.Length != 0 && !Regex.IsMatch(text, @"[\(]$"))
+    //    {
+    //        text = text.Remove(text.Length - 3, 3);
+    //        if (Regex.IsMatch(text, @"[as]$"))
+    //        {
+    //            text = text.Remove(text.Length - 1, 1);
+    //        }
+    //    }
+    //}
+    //        else if (Regex.IsMatch(text, @"(\w)$"))
+    //{
+    //    text = text.Remove(text.Length - 3, 3);
+    //}
+    //Input = text;
+
+
+}
+
+function SwitchSign() {
+    //string text = _input;
+    //if (text.Last() == '-') {
+    //    text = text.Remove(text.Length - 1, 1);
+    //    text += '+';
+    //}
+    //else if (text.Last() == '+') {
+    //    text = text.Remove(text.Length - 1, 1);
+    //    text += "-";
+    //}
+    //Input = text;
+
+}
+
+
+function AddDot() {
+    ///if (!Regex.IsMatch(_input, @"\d+[.]\d+$") && Regex.IsMatch(_input, @"\d+$"))
+    {
+        AddText(".");
+    }
+}
+
+function AddX() {
+    //if (_input.Length == 0 || Regex.IsMatch(_input, @"([\+\-\*/^(]|mod)$"))
+    {
+        AddText("x");
+    }
+}
+
+function AddOBranch() {
+    {
+        //branches++;
+        AddText("(");
+    }
+}
+
+function AddCBranch() {
+    //if (branches != 0 && Regex.IsMatch(_input, @"(\d|[)]|[xX])$"))
+    {
+        //branches--; 
+        AddText(")");
+    }
+}
